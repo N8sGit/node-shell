90a671059de8bddaecb22ac71ebefdd60743d76c
Nathan Anecone <nanecone.gmail.com>
2017-03-06 14:51:23 -0500

    almost finished

--- bash.js
+++ bash.js
@@ -1,14 +1,39 @@
+var done = function(output) {
+  process.stdout.write(output)
+
+  process.stdout.write('prompt > ')
+}
+
+module.exports = {
+  done:done
+}
+
+
+
 var pwd = require('./commands.js')['pwd']
 var fs = require('fs')
 var ls = require('./commands.js')['ls']
+var echo = require('./commands.js')['echo']
+var cat = require('./commands.js')['cat']
+var head = require ('./commands.js')['head']
+var tail = require('./commands.js')['tail']
+var rq = require('./commands.js')['rq']
+
+
 
 process.stdout.write('prompt > ');
 process.stdin.on('data', function (data) {
   var command = data.toString().trim(); // remove the newline
-if(command.toLowerCase() === 'pwd') pwd()
+  var splitData = command.split(' ')
+  command = splitData[0]
+  var args = splitData.slice(1)
+
+  if(command.toLowerCase() === 'pwd') pwd()
+  if(command.toLowerCase() === 'ls') ls()
+  if(command.toLowerCase() === 'echo') echo(args)
+  if(command.toLowerCase() === 'cat') cat(args[0])
+  if(command.toLowerCase() ==='head') head(args[0], args[1])
+  if(command.toLowerCase() === 'tail') tail(args[0],args[1])
+  if(command.toLowerCase()=== 'rq')rq(args[0])
+
 })
-
-
-
-pwd()
-ls()
--- commands.js
+++ commands.js
@@ -1,15 +1,97 @@
 //Output a prompt
+var request = require('request')
 var fs = require('fs')
+var done = require('./bash.js')['done']
 
 
+function rq(param){
+ request(param, function (error, response, body) {
+   done(body); // Print the HTML for the Google homepage.
+});
 
-function ls(){fs.readdir('.', function(err, files) {
+
+}
+
+function ls(param){fs.readdir('.', function(err, files) {
   if (err) throw err;
   files.forEach(function(file) {
-    process.stdout.write(file.toString() + "\n");
+    done(file.toString() + "\n");
   })
-  process.stdout.write("prompt > ");
 });
+
+}
+
+
+function echo(param){
+
+  done(param.join(' '))
+
+}
+
+
+function cat (param){
+  var content
+  fs.readFile(param, function read(err, data){
+    if(err){
+      throw err;
+    }
+    content = data
+    //console.log(Object.keys(content), 'HERE!')
+
+
+   done(content)
+  })
+
+}
+
+
+function head(param, param2){
+  var content
+  fs.readFile(param, function read(err, data){
+    if(err){
+      throw err;
+    }
+     content = data
+
+    if(param2){
+    //  console.log('GOT THERE')
+    var result = content.toString().split('\n').slice(0, param2)
+  }else{
+    // console.log('Else Satement')
+    // console.log(typeof content, 'TYPE OF')
+    var result =  content.toString().split('\n').slice(0, 6)
+    //console.log(result, 'RESULT')
+  }
+
+  done(result.join('\n'))
+})
+
+
+
+}
+
+
+function tail(param, param2){
+  var content
+  fs.readFile(param, function read(err, data){
+    if(err){
+      throw err;
+    }
+     content = data
+
+    if(param2){
+    //  console.log('GOT THERE')
+    var result = content.toString().split('\n').slice(-param2)
+  }else{
+    // console.log('Else Satement')
+    // console.log(typeof content, 'TYPE OF')
+    var result =  content.toString().split('\n').slice(-5)
+    //console.log(result, 'RESULT')
+  }
+
+  done(result.join('\n'))
+  })
+
 
 }
 
@@ -17,21 +99,34 @@ function ls(){fs.readdir('.', function(err, files) {
 
 
 
- function pwd() {
+
+
+ function pwd(param) {
 
 // The stdin 'data' event fires after a user types in a line
 
-   process.stdout.write(process.env['PWD']);
+   done(process.env['PWD']);
   // if(cmd.toLowerCase() === 'date') process.stdout.write(Date())
 
-  process.stdout.write('prompt > ');
+
 
 }
 
 var cmds = {
   pwd: pwd,
 
-  ls: ls
+  ls: ls,
+
+  echo: echo,
+
+  cat: cat,
+
+  head: head,
+
+  tail : tail,
+
+  rq: rq
+
 }
 
 
